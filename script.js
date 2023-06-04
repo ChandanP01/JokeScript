8fcb51f177ebac7c77d66914c2cc269b9bc172df
Chandan Kumar Parida <120441215+ChandanP01@users.noreply.github.com>
2023-06-04 16:03:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,230 @@
+let jokes = [`A pair of cows were talking in the field. One says, “Have you heard about the mad cow disease that’s going around?”
+“Yeah,” the other cow says. “Makes me glad I’m a penguin.”`, `“Once, my father came home and found me in front of a roaring fire. That made my father very mad, as we didn’t have a fireplace.”`,
+    `“Your mother has been with us for 20 years,” said John. “Isn’t it time she got a place of her own?”
+“My mother?” replied Helen. “I thought she was your mother.”`,
+    `"Why don’t pirates take a shower before they walk the plank?"
+"They just wash up on shore."`,
+    `In Denver, the members of a Sunday­-school class were asked to set down their favorite biblical truths. One youngster laboriously printed: “Do one to others as others do one to you.”`,
+    `One of the oddities of Wall Street is that it is the dealer and not the customer who is called broker.`,
+    `Two Hollywood stars ran into each other at the door of their psychiatrist’s office
+“Hello, there,” said one. “Are you coming or going?”
+“If I knew that,” said the other, “I wouldn’t be here.”`,
+    `You can’t believe everything you hear—but you can repeat it.`,
+    `Two bytes meet.  The first byte asks, “Are you ill?”
+The second byte replies, “No, just feeling a bit off.”`,
+    `Eight bytes walk into a bar.  The bartender asks, “Can I get you anything?”
+“Yeah,” reply the bytes.  “Make us a double.”`,
+    `“Knock, knock.”
+“Who’s there?”
+very long pause….
+“Java.”`,
+    `A man is smoking a cigarette and blowing smoke rings into the air.  His girlfriend becomes irritated with the smoke and says, “Can’t you see the warning on the cigarette pack?  Smoking is hazardous to your health!”
+To which the man replies, “I am a programmer.  We don’t worry about warnings; we only worry about errors.”`,
+    `Why do programmers prefer dark mode? Because light attracts bugs.`,
+    `Programmer: An organism that turns coffee into software`,
+    `पत्नी  - सूजी के हलवे में चीनी कम है...!
+
+पति - लेकिन ये तो उपमा बनाया था मैंने...!
+.
+पत्नी - अच्छा, फिर नमक ज्यादा है उपमा में...!
+.
+पति बेहोश...!`,
+    `Why couldn't the sunflower ride its bike? It lost its petals.`,
+    `What's an egg's favorite vacation spot? New Yolk City.`,
+    `What kind of candy do astronauts like? Mars bars.`,
+    `My uncle named his dogs Timex and Rolex. They're his watch dogs.`,
+    `How do you open a banana? With a mon-key.`,
+    `Did you hear about the guy whose left side was cut off? He's all right now.`,
+    `Why does Humpty Dumpty love autumn? Because he had a great fall.`,
+    `What do you call a pudgy psychic? A four-chin teller.`,
+    `How does an octopus go into battle? Well-armed.`,
+    `A termite walks into a bar and says, "So, is the bar tender here?"`,
+    `Why do bees have sticky hair? Because they use a honeycomb.`,
+    `What do you call a hippie's wife? Mississippi.`,
+    `Two peanuts were walking down the street. One was a-salted.`,
+    `What's the stinkiest planet? Poopiter.
+`,
+    `What did the mayonnaise say when the refrigerator door was opened? Close the door, I'm dressing.`,
+    `What did one wall say to the other? I'll meet you at the corner.`,
+    `What did one hat say to the other? You wait here, I'll go on ahead.`,
+    `What did the full glass say to the empty glass? You look drunk.`,
+    `Why don’t we see elephants hiding in trees? Because they’re really good at it.`,
+    `What do you call a boomerang that won’t come back? A stick.`,
+    `Did you hear the rumor about butter? Well, I'm not going to spread it.`,
+    `Why did the student eat his homework? Because his teacher told him it was a piece of cake.`,
+    `Why was the coach yelling at the vending machine? He wanted his quarter back.`,
+    `What's the best smelling insect? A deodor-ant.`,
+    `How do celebrities stay cool? They have many fans.`,
+    `How did the student feel when he learned about electricity? Totally shocked.`,
+    `Why was six afraid of seven? Because 7-8-9.`,
+    `If April showers bring May flowers, what do May flowers bring? Pilgrims.`,
+    `What did the buffalo say when his kid went to college? Bison.`,
+    `How can you tell it’s a dogwood tree? By the bark.`,
+    `I tried to catch fog yesterday. Mist.`,
+    `What do you call a pig that does karate? A pork chop.`,
+    `What do you call a pony with a sore throat? A little horse.`,
+    `Where does Batman go to the bathroom? The batroom.`,
+    `My wife told me to stop acting like a flamingo, so I had to put my foot down.`,
+    `What did the mama tomato say to the baby tomato? Catch up!`,
+    `Why didn't the melons get married? Because they cantaloupe.`,
+    `What did the left eye say to the right eye? Between you and me, something smells.`,
+    `Why didn't the melons get married? Because they cantaloupe.`,
+    `What do you call a fake noodle? An impasta.
+`,
+    `I'm so good at sleeping I can do it with my eyes closed!`,
+    `Which is faster, hot or cold? Hot, because you can catch cold.`,
+    `What do you call a couple of chimpanzees sharing an Amazon account? PRIME-mates`,
+    `What is fast, loud and crunchy? A rocket chip`,
+    `Why do oranges wear sunscreen? So they don't peel.`,
+    `What did one plate say to the other plate? Dinner's on me.`,
+    `What time is it when the clock strikes 13?
+    Time to get a new clock.`,
+    `How does a cucumber become a pickle?
+    It goes through a jarring experience.`,
+    `What do you call a boomerang that won’t come back?
+    A stick.`,
+    ` Why can’t Elsa from Frozen have a balloon?
+    Because she will "let it go, let it go."`,
+    `Why did the dinosaur cross the road?
+    Because the chicken wasn’t born yet.`, ,
+    `What do you call a dog magician?
+    A labracadabrador.`,
+    `Where would you find an elephant?
+    The same place you lost her.`,
+    `How does a scientist freshen her breath?
+    With experi-mints.`,
+    `How are false teeth like stars?
+    They come out at night.`,
+    `What building in your town has the most stories?
+    The public library.`,
+    `What is a computer's favorite snack?
+    Computer chips.`,
+    `What did one volcano say to the other?
+    I lava you.`,
+    `How do we know that the ocean is friendly?
+    It waves.`,
+    `What is a tornado’s favorite game to play?
+    Twister.`,
+    ` How does the moon cut his hair?
+    Eclipse it.`,
+    `How do you talk to a giant?
+    Use big words.`,
+    `What falls in winter but never gets hurt?
+    Snow.`,
+    `What did the Dalmatian say after lunch?
+    That hit the spot.`,
+    ` Why did the kid cross the playground?
+    To get to the other slide.`,
+    `What did the little corn say to the mama corn?
+    Where is pop corn?`,
+    `How do you make a lemon drop?
+    Just let it fall.`,
+    `Why was the baby strawberry crying?
+    Because her mom and dad were in a jam.`,
+    `Why did the cookie go to the hospital?
+    Because he felt crummy.`,
+    `What did the limestone say to the geologist?
+    Don’t take me for granite.`,
+    `What kind of water can’t freeze?
+    Hot water.`,
+    `What kind of tree fits in your hand?
+    A palm tree.`,
+    `What do you call a dinosaur that is sleeping?
+    A dino-snore`,
+    `What is fast, loud and crunchy?
+    A rocket chip.`,
+    `What has ears but cannot hear?
+    A cornfield.`,
+    ` What did the left eye say to the right eye?
+    Between us, something smells.`,
+    `When you look for something, why is it always in the last place you look?
+    Because when you find it, you stop looking.`,
+    `What is brown, hairy and wears sunglasses?
+    A coconut on vacation.`,
+    `What did the tiger say to her cub on his birthday?
+    It's roar birthday.`,
+    `Boy’s definition of a waffle: “A pancake with a nonskid tread.” `,
+    `What do sprinters eat before a race?
+    Nothing. They fast!`,
+    `Why do hummingbirds hum?
+    Because they don’t know the words!`,
+    `What do you call an illegally parked frog?
+    Toad!`,
+    `What does a triceratops sit on?
+    Its tricera-bottom!`,
+    `Knock, knock.
+    Who’s there?
+    Ice cream.
+    Ice cream who?
+    ICE CREAM SO YOU CAN HEAR ME!`,
+    `Knock, knock.
+    Who’s there?
+    Dozen.
+    Dozen who?
+    Dozen anyone want to let me in?`,
+    `Knock, knock.
+    Who’s there?
+    Water.
+    Water who?
+    Water you asking so many questions for, just open up!`,
+    `What do you call a sad strawberry?
+    A blueberry!`,
+    `What happens when a grape gets run over crossing the street?
+    A traffic jam!`,
+    `What does a lemon say when it answers the phone?
+    Yellow!`,
+    `Why do bees have sticky hair?
+    They use honeycombs!`,
+    `What do you call a number that can’t stay in one place?
+    A roamin’ numeral!`,
+    `What did the 90° angle say after an argument?
+    "It turns out, I was right!"`,
+    `What is a bird’s favorite type of math?
+    OWL-gebra!`,
+    `Why does it take pirates a long time to learn the alphabet?
+    Because they can spend years at C!`,
+    `What did one wall say to the other wall?
+I’ll meet you at the corner!`,
+
+    `Why did the kid throw his clock out the window?
+Because he wanted to see time fly!`,
+
+    `What do you call a huge pile of cats?
+A meow-ntain!`,
+
+    `Why wouldn’t the shrimp share his treasure?
+Because he was a little shellfish!`,
+
+    `Why is Cinderella bad at soccer?
+Because she’s always running away from the ball!`,
+
+    `What does a rain cloud wear under its clothes?
+Thunderwear!`,
+
+    `Why do vampires seem sick all the time?
+Because they’re always coffin!`,
+
+    `How do you get an astronaut’s baby to stop crying?
+You rocket!`,
+    `What’s the difference between a guitar and a fish?You can tune a guitar, but you can’t tuna fish!`,
+
+    `What did the fisherman say to the magician?
+Pick a cod, any cod!`,
+
+    `Why did the picture go to prison?
+Because it was framed!`,
+
+    `Why do bicycles fall over?
+Because they’re two-tired!`,
+
+    `If a seagull flies over the sea, what flies over the bay?
+A bagel!`
+]
+
+const tellJoke = () => {
+    let jokeNo = Math.floor(Math.random() * (jokes.length - 1));
+    document.getElementsByTagName('p')[0].innerHTML = jokes[jokeNo]
+    document.getElementsByTagName('button')[0].innerHTML = "click again to see joke"
+}
+
+console.log(jokes.length);
